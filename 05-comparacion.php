8063b5d0f0bd020892e3fa9df6f6dc5300efb9d8
salgadoIbacache <se.salgado@duocuc.cl>
2023-11-21 13:25:54 -0300

    Mayor que, menor que , mayor o igual, si el numero de la izquierda es mayor que el de la derecha

--- 05-comparacion.php
+++ 05-comparacion.php
@@ -1,32 +1,34 @@
 <?php include 'includes/header.php';
 
+
+// Creamos variables numericos
 $numero1 = 20;
 $numero2 = 30;
 $numero3 = 30;
 $numero4 = "30";
 
-var_dump($numero1 > $numero2);
+var_dump($numero1 > $numero2);// Si es mayor que
 echo "<br/>";
 
-var_dump($numero1 < $numero2);
+var_dump($numero1 < $numero2);// Si es menor que
 echo "<br/>";
 
-var_dump($numero1 >= $numero2);
+var_dump($numero1 >= $numero2);// Si es mayor o igual
 echo "<br/>";
 
-var_dump($numero1 <= $numero2);
+var_dump($numero1 <= $numero2);// Si es menor o igual
 echo "<br/>";
 
-var_dump($numero2 == $numero3);
+var_dump($numero2 == $numero3);// Si es igual 
 echo "<br/>";
 
-var_dump($numero2 == $numero4);
+var_dump($numero2 == $numero4);// Si es igual
 echo "<br/>";
 
-var_dump($numero2 === $numero4);
+var_dump($numero2 === $numero4);// Este revisa el valor y el tipo de datos
 echo "<br/>";
 
-// -1 Si Izquierda es menor, 0 Si es igual, 1 Si izquierda es mayor
+// -1 Si el numero de la Izquierda es menor, 0 Si es igual, 1 Si izquierda es mayor
 var_dump($numero1 <=> $numero2);
 echo "<br/>";
 
