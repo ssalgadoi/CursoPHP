f90b41e2eb6a3c3ea862cf0088f7410ad39fb270
salgadoIbacache <se.salgado@duocuc.cl>
2023-11-21 13:06:18 -0300

    Realizamos Variables en PHP

--- 02-variables.php
+++ 02-variables.php
@@ -1,21 +1,21 @@
 <?php include 'includes/header.php';
 
 
-$nombre = "Juan";
+$nombre = "Juan";// Variables en PHP
 
 
 $nombre = "Juan 2";
 
-echo $nombre;
-var_dump($nombre);
+echo $nombre;// Imprimer la variable
+var_dump($nombre);// Nos da la informacion de la variable como si es string o int
 
-define('constante', "Este es el valor de la constante");
-echo constante;
+define('constante', "Este es el valor de la constante");// Esta es una constante
+echo constante;// Este imprime la constante
 
 const constante2 = "Hola 2";
 echo constante2;
 
-$nombreCliente = "Pedro";
-$nombre_cliente = "Pedro";
+$nombreCliente = "Pedro";// Variables con don palabras
+$nombre_cliente = "Pedro";// Esta se ocupa en php
 
 include 'includes/footer.php';
