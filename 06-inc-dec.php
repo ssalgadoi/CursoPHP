7d51fd2c56a0bcc56139b9522aa550bdda497859
salgadoIbacache <se.salgado@duocuc.cl>
2023-11-21 13:30:14 -0300

    Incremento de numero ++, += y --

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -2,10 +2,10 @@
 
 
 $numero1 = 30;
-echo ++$numero1;
+echo ++$numero1;// Con esto invrementamos los numeros
 
 $numero1 += 5;
-echo $numero1;
+echo $numero1;// Al 30 le suma 6
 
 $numero2 = 30;
 echo --$numero2;
