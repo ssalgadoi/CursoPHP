5def85156775f82acf2fa611982ed17a20db9465
salgadoIbacache <se.salgado@duocuc.cl>
2023-11-21 13:15:58 -0300

    Operadores Suma, Resta, Multiplicar, Dividir y Multiplicar

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,10 +1,14 @@
 <?php include 'includes/header.php';
 
+
+// Variables
 $numero1 = 20;
 $numero2 = 10;
 
 // Sumar
-echo $numero1 + $numero2;
+echo $numero1 + $numero2;// echo para imprimir 
+
+echo "<br>";
 
 // Resta
 echo $numero1 - $numero2;
@@ -14,8 +18,12 @@ echo "<br>";
 // Multiplicar
 echo $numero1 * $numero2;
 
+echo "<br>";
+
 // Dividir
 echo $numero1 / $numero2;
+
+echo "<br>";
 
 // Multiplicar cierta cantidad de veces
 echo 2 ** 3;
